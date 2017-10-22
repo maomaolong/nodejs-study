f816c59fd5019ab1ff293211f49daf42a652a559
maomaolong <562522970@qq.com>
2017-10-22 15:37:55 +0800

    change model-fs.js

--- model-fs.js
+++ model-fs.js
@@ -7,3 +7,12 @@ fs.readFile("./README.md",function(err,data){
 		console.log(data.toString())
 	}
 })
+
+fs.stat("./README.md",function(err,stat){
+	if (err) {
+		console.log(err)
+	}else{
+		console.log("stat.isFile()",stat.isFile())
+		console.log("stat.isDirectory()",stat.isDirectory())
+	}
+})
